70bd6dd65fe993518169f523ac263fb6b28efdc2
Matthias Eggert <matthias.eggert@sogeti.com>
2025-08-15 10:31:00 +0200

    full screen pics

--- web/src/app/components/ItemList.tsx
+++ web/src/app/components/ItemList.tsx
@@ -346,17 +346,14 @@ export default function ItemList({ categoryId }: PropsList) {
           className="fixed inset-0 z-50 bg-black bg-opacity-80 flex items-center justify-center"
           onClick={() => setModalImage(null)}
         >
-          <div className="w-full max-w-screen-lg px-4">
-            <Image
-              src={modalImage}
-              alt="Full size"
-              unoptimized
-              width={0}
-              height={0}
-              sizes="100vw"
-              className="w-full h-auto rounded-xl shadow-lg object-contain"
-            />
-          </div>
+          <Image
+            src={modalImage}
+            alt="Full size"
+            width={1000}
+            height={1000}
+            unoptimized
+            className="max-w-full max-h-full rounded-xl shadow-lg"
+          />
         </div>
       )}
     </div>
